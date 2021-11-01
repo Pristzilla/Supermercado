a1c57383de921399479f0172b322a7328ae9cf0c
Javier <javier.barquine@gmail.com>
2021-11-01 12:18:07 +0100

    Cambio nombres en metodos de la interfaz para adecuarlos al diagrama UML

--- Supermercado/SupermercadoCommon/src/main/java/es/unican/ps/supermercado/daoLayer/IArticulosDAO.java
+++ Supermercado/SupermercadoCommon/src/main/java/es/unican/ps/supermercado/daoLayer/IArticulosDAO.java
@@ -9,10 +9,6 @@ public interface IArticulosDAO {
 	public Articulo crearArticulo (Articulo a);
 	public Articulo modificarArticulo (Articulo a);
 	public Articulo eliminarArticulo (Articulo a);
-<<<<<<< .merge_file_DkUmXR
-	public Articulo buscarArticuloPorID (Long id);
-=======
 	public Articulo buscarArticuloPorId (Long id);
->>>>>>> .merge_file_VrnMhQ
 	public List<Articulo> articulos();
 }
--- Supermercado/SupermercadoCommon/src/main/java/es/unican/ps/supermercado/daoLayer/IPedidosDAO.java
+++ Supermercado/SupermercadoCommon/src/main/java/es/unican/ps/supermercado/daoLayer/IPedidosDAO.java
@@ -9,12 +9,7 @@ public interface IPedidosDAO {
 	public Pedido crearPedido (Pedido p);
 	public Pedido modificarPedido (Pedido p);
 	public Pedido eliminarPedido (Pedido p);
-<<<<<<< .merge_file_fqee7p
-	public Pedido buscarPedidoPorID (Long id);
-=======
 	public Pedido buscarPedidoPorId (Long id);
->>>>>>> .merge_file_TCcVNu
 	public Pedido buscarPedidoPorReferencia (String ref);
-
 	public List<Pedido> Pedidos();
 }
