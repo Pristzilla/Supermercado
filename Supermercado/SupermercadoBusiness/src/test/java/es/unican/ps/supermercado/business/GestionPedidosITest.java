046083e035d3670011efd6fae7dbe2077993d621
Javier <javier.barquine@gmail.com>
2021-12-09 21:26:32 +0100

    Arreglandote los problemas como siempre, tu fiel programador

--- Supermercado/SupermercadoBusiness/src/test/java/es/unican/ps/supermercado/business/GestionPedidosITest.java
+++ Supermercado/SupermercadoBusiness/src/test/java/es/unican/ps/supermercado/business/GestionPedidosITest.java
@@ -8,11 +8,16 @@ package es.unican.ps.supermercado.business;
 
 import static org.junit.Assert.*;
 
+import javax.ejb.embeddable.EJBContainer;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import es.unican.ps.supermercado.businessLayer.IGestionPedidosRemote;
+import es.unican.ps.supermercado.daoLayer.IArticulosDAORemote;
 
 /**
  * @author barquinj
@@ -20,7 +25,15 @@ import org.junit.Test;
  */
 public class GestionPedidosITest {
 
-
+	private static EJBContainer ec;
+	private static IArticulosDAORemote articulosDAO;
+	private static IGestionPedidosRemote sut;
+	
+	@BeforeClass
+	public static void initContainer() throws Exception {
+		
+	}
+	
 	/**
 	 * @throws java.lang.Exception
 	 */
--- Supermercado/SupermercadoDAO/src/test/java/es/unican/ps/supermercado/daoLayer/PedidosDAOTest.java
+++ Supermercado/SupermercadoDAO/src/test/java/es/unican/ps/supermercado/daoLayer/PedidosDAOTest.java
@@ -1,5 +1,8 @@
 package es.unican.ps.supermercado.daoLayer;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 // import static org.mockito.Mockito.*;
 import org.junit.Before;
 import org.junit.Test;
